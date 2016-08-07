d4e6b2adc56725eda1b1d32ecc54b06f04c3bd48
kdontula <kdontula@hawk.iit.edu>
2016-08-07 14:25:19 -0500

    initial commit

---
+++ app/src/main/java/com/kaushal/scale/ScaleActivity.java
@@ -0,0 +1,25 @@
+package com.kaushal.scale;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ScaleActivity extends Activity {
+
+    private TextView txtValue;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.scale);
+        final ScaleView rulerViewMm = (ScaleView) findViewById(R.id.my_scale);
+        txtValue = (TextView) findViewById(R.id.txt_height);
+        rulerViewMm.setStartingPoint(1);
+        rulerViewMm.setUpdateListener(new onViewUpdateListener() {
+            @Override
+            public void onViewUpdate(float result) {
+                float value = (float) Math.round(result * 10f) / 10f;
+                txtValue.setText(value + " cm");
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/kaushal/scale/onViewUpdateListener.java
@@ -0,0 +1,8 @@
+package com.kaushal.scale;
+
+/**
+ * Created by xkxd061 on 8/4/16.
+ */
+public interface onViewUpdateListener {
+    void onViewUpdate(float value);
+}
